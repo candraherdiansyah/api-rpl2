2a7f84762c3612fd0445c96f6b798e02190c9cdb
candra <candraherdiansyah14@gmail.com>
2024-07-30 17:01:10 +0700

    berita 50%

---
+++ app/Http/Controllers/Api/BeritaController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Berita;
+use Illuminate\Http\Request;
+use Str;
+use Validator;
+
+class BeritaController extends Controller
+{
+
+    public function index()
+    {
+        $berita = Berita::with('kategori', 'tag', 'user')->latest()->get();
+        return response()->json([
+            'success' => true,
+            'message' => 'daftar berita',
+            'data' => $berita,
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'judul' => 'required|unique:beritas',
+            'deskripsi' => 'required',
+            'foto' => 'required|image|mimes:png,jpg|max:2048',
+            'id_kategori' => 'required',
+            'tag' => 'required|array',
+            'id_user' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'validasi gagal',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        try {
+            // upload foto
+            $path = $request->file('foto')->store('berita');
+
+            $berita = new Berita;
+            $berita->judul = $request->judul;
+            $berita->slug = Str::slug($request->judul);
+            $berita->deskripsi = $request->deskripsi;
+            $berita->foto = $path;
+            $berita->id_user = $request->id_user;
+            $berita->id_kategori = $request->id_kategori;
+            $berita->save();
+
+            // melampirkan banyak tag
+            $berita->tag()->attach($request->tag);
+            return response()->json([
+                'success' => true,
+                'message' => 'berita berhasil dibuat',
+                'data' => $berita,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'terjadi kesalahan',
+                'errors' => $e->getMessage(),
+            ], 500);
+
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
