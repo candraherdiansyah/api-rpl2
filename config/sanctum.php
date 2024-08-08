1c38423abf619ca156917cd5f2ef7375ea9def3e
candra <candraherdiansyah14@gmail.com>
2024-08-08 08:57:54 +0700

    hahah

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,https://movieapikc.69dev.id',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,movieapikc.69dev.id',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
